3362ae47af1cd23a6a61b2d033afe8b26acd2359
Istvan Szalonti <istvan.szalonti12@gmail.com>
2018-02-15 19:51:58 +0100

    template added

--- doublecountdown/app.js
+++ doublecountdown/app.js
@@ -3,3 +3,16 @@
  *
  */
 
+const express = require('express')
+const app = express()
+const PORT = 3000;
+
+//app.set('views', './views')
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there' })
+})
+
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
