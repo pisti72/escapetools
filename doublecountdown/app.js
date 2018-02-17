cf9a2440370b152932ef70a471aacf5bfca5d723
Istvan Szalonti <istvan.szalonti12@gmail.com>
2018-02-17 19:46:06 +0100

    digits added

--- doublecountdown/app.js
+++ doublecountdown/app.js
@@ -4,12 +4,21 @@
  */
 
 const express = require('express')
-var ip = require("ip");
+var ip = require("ip")
 const app = express()
 const PORT = 3000;
+const ONSTART = 0
+const ONSTOP = 2
+const INAIR = 3
+const INGAME = 4
+const GAMEOVER = 5
+const GAMEFINISHED = 6
+const MAXHINTS = 3
+const MAXTOP = 12
 
+var gameStatus = ONSTART
+var hints = 0
 
-//app.set('views', './views')
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
@@ -18,9 +27,88 @@ app.get('/', (req, res) => {
 })
 
 app.get('/getmyip', (req, res) => {
-    result = { ip: 0 }
-    result.ip = ip.address()
+    var result = { ip: ip.address(), port: PORT }
     res.send(JSON.stringify(result))
 })
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+app.get('/start', (req, res) => {
+    gameStarted()
+    var response = 'Started at: ' + startedAt
+    console.log(response)
+    res.send(response)
+})
+
+app.get('/stop', (req, res) => {
+    var d = new Date()
+    var time = d.getTime() - startedAt
+    var response = 'Your time: ' + time + '  in readable: ' + getReadable(time)
+    res.send(response)
+})
+
+app.get('/getgamedata', (req, res) => {
+    var d = new Date()
+    var time;
+    if (gameStatus == ONSTART) {
+        time = 0
+    } else if (gameStatus == INGAME) {
+        time = d.getTime() - startedAt
+    } else if (gameStatus == GAMEOVER || gameStatus == GAMEFINISHED) {
+        time = stoppedAt - startedAt
+    }
+    //TEST
+    //time = 999999;
+    //gameStatus = GAMEFINISHED;
+    var response = { time: getReadable(time), ms: time, hints: hints, status: gameStatus }
+    res.send(JSON.stringify(response))
+})
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT + '!'))
+
+
+/**
+ * 
+ * HELPER FUNCTIONS
+ * 
+ */
+
+
+function getHHHHMMDDfromTimestamp(date) {
+    var d = new Date(date)
+    var dd = d.getDate();
+    var mm = d.getMonth() + 1;
+    var yyyy = d.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    return yyyy + '/' + mm + '/' + dd;
+}
+
+function gameStarted() {
+    var d = new Date()
+    gameStatus = INGAME
+    hints = 0
+    startedAt = d.getTime()
+}
+
+function getReadable(time) {
+    var t = new Date(time)
+    return two(t.getMinutes()) + ':' + two(t.getSeconds()) + ':' + two(t.getMilliseconds())
+}
+
+function two(n) {
+    var s = n + ''
+    if (s.length == 1) {
+        return '0' + s
+    }
+    if (s.length == 2) {
+        return s
+    }
+    if (s.length > 2) {
+        return s.substr(0, 2)
+    }
+}
+
+
--- doublecountdown/public/functions.js
+++ doublecountdown/public/functions.js
@@ -1,17 +1,97 @@
-function test(){
-    fetch('/getmyip').then(function(response){
-        return response.json()        
-    }).then(function(j){
-        f('message').innerHTML = j.ip;
-    }).catch(function(error){
+var thread;
+var fullscreen=false;
+var client = 0;
+
+function test() {
+    fetch('/getmyip').then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        f('message').innerHTML = json.ip + ':' + json.port;
+    }).catch(function (error) {
         f('message').innerHTML = 'Server not available';
     })
-    
+
 }
-function start(n){
+function client(n) {
     f('server').style.display = 'none';
     f('client').style.display = 'block';
+    client = n;
+    thread = setInterval('loop()', 50);
 }
-function f(n){
+
+function hint(){
+    fetch('/start').then().catch(function (error) {
+        console.log(error)
+    })
+    f('message').innerHTML = 'Game started';
+}
+
+function start(){
+    fetch('/start').then().catch(function (error) {
+        console.log(error)
+    })
+    f('message').innerHTML = 'Game started';
+}
+
+function pause(){
+    fetch('/pause').then().catch(function (error) {
+        console.log(error)
+    })
+    f('message').innerHTML = 'Game paused';
+}
+
+function loop() {
+    fetch('/getgamedata').then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        var time = json.time
+        f('d5').innerHTML = time.substr(0, 1);
+        f('d4').innerHTML = time.substr(1, 1);
+        f('d3').innerHTML = time.substr(3, 1);
+        f('d2').innerHTML = time.substr(4, 1);
+        f('d1').innerHTML = time.substr(6, 1);
+        f('d0').innerHTML = time.substr(7, 1);
+    }).catch(function (error) {
+        f('clock').innerHTML = 'Server not available';
+    })
+}
+
+function toggleFullscreen() {
+    if (
+        document.fullscreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled) {
+        var d = document.body;
+        if (!fullscreen) {
+            fullscreen = true;
+            //f('fullscreen').innerHTML = EMPTY;
+            //f('fullscreen').style.display = 'none';
+            if (d.requestFullscreen) {
+                d.requestFullscreen();
+            } else if (d.webkitRequestFullscreen) {
+                d.webkitRequestFullscreen();
+            } else if (d.mozRequestFullScreen) {
+                d.mozRequestFullScreen();
+            } else if (d.msRequestFullscreen) {
+                d.msRequestFullscreen();
+            }
+        } else {
+            fullscreen = false;
+            //f('fullscreen').style.display = 'block';
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+        }
+    }
+}
+
+function f(n) {
     return document.getElementById(n);
 }
