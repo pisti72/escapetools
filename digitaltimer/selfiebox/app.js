4a1466f8a02bc8906fcd397db06d23a447789553
Istvan Szalontai <istvan.szalontai12@gmail.com>
2019-10-08 11:59:06 +0200

    nicer

--- digitaltimer/selfiebox/app.js
+++ digitaltimer/selfiebox/app.js
@@ -1,57 +1,90 @@
 const url = 'api.php';
 const STATE_WELCOME = 0;
+const STATE_SHOT = 1;
+const STATE_PREVIEW = 2;
+const STATE_SEND = 3;
 var counter = 0;
 var state = STATE_WELCOME;
 var canvas = f('canvas');
 var video = f('video');
 var imageToSend = {};
-var promise = navigator.mediaDevices.getUserMedia({video:true})
-.then(function(stream) {
-  /* use the stream */
-  video.srcObject = stream;
-  video.onloadedmetadata = function(e) {
-    video.play();
-  };
-})
-.catch(function(err) {
-  /* handle the error */
-  console.log(err.name + ": " + err.message);
+var promise = navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        /* use the stream */
+        video.srcObject = stream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
+    })
+    .catch(function (err) {
+        /* handle the error */
+        console.log(err.name + ": " + err.message);
+    });
+
+f('container').addEventListener("click", function () {
+    if (state == STATE_WELCOME) {
+        state = STATE_SHOT;
+        hide('welcome');
+        show('shot');
+    }
 });
 
-//setInterval(function(){animation()},1000);
+setInterval(function () { animation() }, 1000);
 
-function shot(){
-	canvas.width = video.videoWidth;
-	canvas.height = video.videoHeight;
-	ctx = canvas.getContext('2d');
-	ctx.drawImage(video, 0, 0);
+function shot() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0);
     ctx.font = "30px Arial";
     ctx.fillStyle = "#fff";
-    ctx.fillText("www.morrisons.hu",10,40);
-	imageToSend = canvas.toDataURL('image/jpeg', 0.8);
+    ctx.fillText("www.morrisons2.hu", 10, 40);
+    imageToSend = canvas.toDataURL('image/jpeg', 0.8);
+    if (state == STATE_SHOT) {
+        state = STATE_PREVIEW;
+        hide('shot');
+        show('preview');
+    }
 }
 
-function save(){
+function good() {
+    if (state == STATE_PREVIEW) {
+        state = STATE_SEND;
+        hide('preview');
+        show('send');
+    }
+}
+
+function back() {
+    if (state == STATE_PREVIEW) {
+        state = STATE_SHOT;
+        hide('preview');
+        show('shot');
+    }
+}
+
+function save() {
     download("my_picture.jpg", imageToSend);
 }
-function download(filename, image) {
-  var element = document.createElement('a');
-  //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('href', image);
-  element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+function download(filename, image) {
+    var element = document.createElement('a');
+    //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', image);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
 
-function animation(){
+function animation() {
     counter++;
-    if(counter%2==0){
+    if (counter % 2 == 0) {
         f('title_txt').innerHTML = 'Selfie box';
         f('touch_txt').innerHTML = 'Touch the screen';
-    }else{
+    } else {
         f('title_txt').innerHTML = 'Szelfi doboz';
         f('touch_txt').innerHTML = 'Éríntsd meg a képernyőt';
     }
@@ -59,14 +92,14 @@ function animation(){
 
 function send() {
     var email = f('email').value;
-    //var image = '/9j/4AAQSkZJRgABAQEBLAEsAAD//gATQ3JlYXRlZCB3aXRoIEdJTVD/4gKwSUNDX1BST0ZJTEUAAQEAAAKgbGNtcwQwAABtbnRyUkdCIFhZWiAH4wAKAAcADQA4AC1hY3NwQVBQTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA9tYAAQAAAADTLWxjbXMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1kZXNjAAABIAAAAEBjcHJ0AAABYAAAADZ3dHB0AAABmAAAABRjaGFkAAABrAAAACxyWFlaAAAB2AAAABRiWFlaAAAB7AAAABRnWFlaAAACAAAAABRyVFJDAAACFAAAACBnVFJDAAACFAAAACBiVFJDAAACFAAAACBjaHJtAAACNAAAACRkbW5kAAACWAAAACRkbWRkAAACfAAAACRtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACQAAAAcAEcASQBNAFAAIABiAHUAaQBsAHQALQBpAG4AIABzAFIARwBCbWx1YwAAAAAAAAABAAAADGVuVVMAAAAaAAAAHABQAHUAYgBsAGkAYwAgAEQAbwBtAGEAaQBuAABYWVogAAAAAAAA9tYAAQAAAADTLXNmMzIAAAAAAAEMQgAABd7///MlAAAHkwAA/ZD///uh///9ogAAA9wAAMBuWFlaIAAAAAAAAG+gAAA49QAAA5BYWVogAAAAAAAAJJ8AAA+EAAC2xFhZWiAAAAAAAABilwAAt4cAABjZcGFyYQAAAAAAAwAAAAJmZgAA8qcAAA1ZAAAT0AAACltjaHJtAAAAAAADAAAAAKPXAABUfAAATM0AAJmaAAAmZwAAD1xtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAEcASQBNAFBtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEL/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAApAEADAREAAhEBAxEB/8QAGQABAQEBAQEAAAAAAAAAAAAAAwQFAgEA/8QAFwEBAQEBAAAAAAAAAAAAAAAAAQACA//aAAwDAQACEAMQAAAB2A9nys1c6mrXjqkoBkNA12dSIusaEd1OMpuRLMvErrFVlGiEzcsJXs/I5V6xgaAFRTWXNOU/TnoY1iJJNdWiicI9f//EACIQAAICAgICAgMAAAAAAAAAAAECAAMREhAhBCIxMhMjQv/aAAgBAQABBQLjIE8i0iAWPN7KpW4dNV4tcqGBIoGWPUb9k8X1Xi2ZOgUpYq4ilZVjf448izUbB1uPoj/mh1BTImZn1vPddjVl3LsqaxO4W2VT3/N/zKPtfKvo8M//xAAcEQACAwADAQAAAAAAAAAAAAAAARARIAIDITH/2gAIAQMBAT8BxTGsIoUfSpWXKhieH4cYuLGyzsFLx//EABwRAAICAgMAAAAAAAAAAAAAAAABECARMQIhMP/aAAgBAgEBPwGnV3GvFVRqiHGIaEjBxHKhR//EACQQAAIBBAECBwAAAAAAAAAAAAABMQIQESFRInESIDJBgZGh/9oACAEBAAY/Ar6Mo2KpEWWPckq8Uq2B0vm6ZtYFl+oZjOyq+pMraFyL9REDq5JM20QJydWzpg215H2PoXyPvb//xAAhEAEAAgIBBAMBAAAAAAAAAAABABEhMUEQUWGBcaGxwf/aAAgBAQABPyEJUdhnhfhjWY83LgtfHDOZCLNpXzLgAMqpnmUFgIUC9pQLA95JbYjqLQFpcWtYup4KWIoC7lhojlMzHL10wujMKkU7THPO/iAQuSB0hHYMZNOUL3RDeKV72M7jVqd+F40XKAfWM1B8ot7lcJjfeP7Zp6fZzT3H1v1NPk/s0Z//2gAMAwEAAgADAAAAEOkPbRqAKBRfp5o3Z5IzDRpmtP/EABwRAQEBAAMBAQEAAAAAAAAAAAEAERAhMUEgUf/aAAgBAwEBPxDjLyE7kPfwNgQz22/icH4BYjsdWk/ljx9Q72T6gS6LNJQ4l1DjO7f5G/Yc6lPt6Lzx8vJwzf/EABwRAAMAAwEBAQAAAAAAAAAAAAABERAhMVEgQf/aAAgBAgEBPxCEwtjaaE0+fDKNhfoteXipoeyMQuGieibGqHWWMSbNhBqignp4jbshcOMl1HbH3BH/xAAkEAEAAgIBBAICAwAAAAAAAAABABEhMUFRYXGBsfAQ4aHB0f/aAAgBAQABPxAauUmjTyxZFepTb6ldL+VLgO6vL0QWKBkeGKXDkXAGm3tGiFZH6xudo+YwVWDuEFEoHBDyHAfeqOjRX9RHiAGipwL8ev1LqlszrviaigtFdz/PcD3Wp09yraTSDtjzxZCnlr9RUoPlLgFQ0Vvo6+Y54wkUeiWVGMPidQOINADM1ackQKHPVC+eYSMzlCXiYgFd6uGGBrPmWVsK03mGirUWMN0lChCGUQ4ZBvrZLxaDkU695leqtTgV2pmYssuBs9nM2fTE+TGfWdSb/rln0HR+Kv4ZP//Z';
     sendmail(email, imageToSend);
 }
 
 function sendmail(email, image) {
+    var blob = image.replace('data:image/jpeg;base64,', '');
     var data = {
         email: email,
-        image: image
+        image: blob
     };
 
     fetch(url, {
@@ -86,12 +119,27 @@ function sendmail(email, image) {
         }
         f('email').value = '';
         setTimeout(
-            function () { f('message').innerHTML = ''; }, 2000
-        );
+            function () {
+                f('message').innerHTML = '';
+                if (state == STATE_SEND) {
+                    state = STATE_WELCOME;
+                    hide('send');
+                    show('welcome');
+                }
+            }, 4000);
     }).catch(function (error) {
         console.log(error);
     })
 }
+
+function hide(n) {
+    f(n).style.display = 'none';
+}
+
+function show(n) {
+    f(n).style.display = 'block';
+}
+
 function f(n) {
     return document.getElementById(n);
 }
